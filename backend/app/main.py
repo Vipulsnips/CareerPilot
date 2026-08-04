7947f696f516bb2cde02b705534599e38b36bc79
Vipulsnips <vipulrawat137@gmail.com>
2026-08-04 13:14:11 +0530

    nitialize CareerPilot backend with FastAPI PDF extraction

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers.resume import router as resume_router
+
+app = FastAPI()
+
+app.include_router(resume_router)
+
+@app.get('/')
+def root():
+    return {"message": "Server Started"}
